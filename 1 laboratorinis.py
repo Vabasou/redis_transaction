32e7a737b3dac84fb41041b2ffa5792f38fb6e42
Vabasou <beiga.vytautas@gmail.com>
2022-09-28 18:15:17 +0300

    Pataisytas

--- 1 laboratorinis.py
+++ 1 laboratorinis.py
@@ -6,28 +6,27 @@ r = redis.Redis(host='localhost', port=49153, password="redispw")
 
 def makeMultipleTransactions():
     print("Make transaction: ")
-    while (True):
+    makeNewTransaction = True
+    while (makeNewTransaction):
         sender, receiver, amount = getSenderAndReceiver()
         makeTransaction(sender, receiver, amount)
         print("Make another transacton? Y, N: ")
         makeAnother = input()
         if (makeAnother == "N" or makeAnother == "n"):
-            break
-            
+            makeNewTransaction = False
+
 def makeTransaction(senderKey, receiverKey, amount):
     p = r.pipeline()
-    p.watch(senderKey)
     if (checkStudentBalance(senderKey, amount)):
-        p.multi()
+        p.watch(senderKey)
         senderMoney = p.get(senderKey)
         receiverMoney = p.get(receiverKey)
+        p.multi()
         p.set(senderKey, float(senderMoney) - float(amount))
         p.set(receiverKey, float(receiverMoney) + float(amount))
-        senderBalance = p.get(senderKey)
-        receiverBalance = p.get(receiverKey)
         p.execute()
-        print("Sender balance: " + senderBalance)
-        print("Receiver balance: " + receiverBalance)
+        print("Sender balance: " + str(getStudentBalance(senderKey)))
+        print("Receiver balance: " + str(getStudentBalance(receiverKey)))
         return True
     else:
         print("No money, no honey")
@@ -70,7 +69,7 @@ def consoleUser():
     surname = input()
     print("Type user balance")
     balance = input()
-    
+
     return name, surname, balance
 
 def getStudentKey(name, surname): #2 different variables if two students with same name/surname appeared
@@ -79,37 +78,38 @@ def getStudentKey(name, surname): #2 different variables if two students with sa
 
 def createUsers():
     print("Creating users...")
-    while (True):
+    createNewUser = True
+    while (createNewUser):
         name, surname, balance = consoleUser()
-        if (name == "" or surname == "" or studentExist(studentKey) == 1):
-            print("Name is empty or already picked")
+        studentKey = getStudentKey(name, surname)
+        if (name == "" or surname == ""):
+            print("Name cannot be empty")
         else:
-            studentKey = getStudentKey(name, surname)
             print(studentKey + " Praejo")
-            setValueForKey(studentKey, balance)
-            print(getValueForKey(studentKey))
+            setStudent(studentKey, balance)
+            print(getStudentBalance(studentKey))
             print(r.keys())
             print("Add another student? Y, N: ")
             addAnother = input()
             if (addAnother == "N" or addAnother == "n"):
-                break
+                createNewUser = False
 
 # Helper functions
-def setValueForKey(key, balance):
+def setStudent(key, balance):
     return r.set(key, balance)
 
-def getValueForKey(key):
+def getStudentBalance(key):
     return r.get(key)
 
 def checkStudentBalance(key, amount):
-    return float(getValueForKey(key)) >= float(amount)
-    
+    return (float(getStudentBalance(key)) >= float(amount))
+        
 def studentExist(key):
     return r.exists(key) == 1
 
 def checkAmount(amount):
     if (type(amount) == int or type(amount) == float):
-        return amount > 0
+        return (amount > 0)
     else:
         return False
 
@@ -117,6 +117,6 @@ def checkAmount(amount):
 def main():
     createUsers()
     makeMultipleTransactions()
-    
+
 if __name__ == "__main__":
     main()
